9404fdd4ce43c4af33eea94caa84ac1d460ba167
Shaaamila <shamila.m09@gmail.com>
2023-03-05 14:27:32 -0800

    working on post feed

--- main/views.py
+++ main/views.py
@@ -13,7 +13,9 @@ def index(request):
     # return HttpResponse('<h1>Nice job</h1>')
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html', {'user_profile': user_profile})
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts' : posts})
 
 @login_required(login_url='signin')
 def upload(request):
